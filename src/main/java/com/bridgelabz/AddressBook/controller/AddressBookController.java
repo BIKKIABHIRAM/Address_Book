2d8bd8d40befb634b0944c274267ef89ce740bab
Bikki abhiram <121655216+BIKKIABHIRAM@users.noreply.github.com>
2025-05-20 16:45:48 +0530

    Update AddressBookController.java

--- src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/persons")
+@RequestMapping("/api/contacts")
 public class AddressBookController {
 
     @Autowired
